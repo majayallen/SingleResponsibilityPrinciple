c1b632fb4506e817b0a78ac8527d3d2f90dd3a33
majayallen <majayallen@gmail.com>
2025-06-04 14:46:45 +0330

    SRP

---
+++ SingleResponsibilityPrinciple/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class CustomerRepository
+    {
+        public void Add(Customer customer)
+        {
+            try
+            {
+                // Add customer to Database 
+            }
+            catch (Exception e)
+            {
+                FileLogger.Log(e.ToString());
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
